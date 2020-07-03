4ef5609c3c85000a3b2b7ec6c2bb44f0164e437f
Pietro Oliveira <pietro.dias@fornecedores.klabin.com.br>
2020-07-03 18:58:16 -0300

    RadioButton Finalizado + Login Visitante (85%)

--- src/app/login-visitante/login-visitante.page.ts
+++ src/app/login-visitante/login-visitante.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonSlides } from '@ionic/angular';
+import { IonSlides, IonCheckbox, IonRadio, IonToggle } from '@ionic/angular';
 
 @Component({
   selector: 'app-login-visitante',
@@ -8,6 +8,7 @@ import { IonSlides } from '@ionic/angular';
 })
 export class LoginVisitantePage implements OnInit {
 @ViewChild(IonSlides) slides: IonSlides;
+@ViewChild(IonToggle) toggle: IonToggle;
 
   constructor() { }
 
@@ -23,6 +24,28 @@ export class LoginVisitantePage implements OnInit {
   {
    this.slides.slidePrev();
   }
-
 }
+
+  
+  checkboxRadio() {
+    console.log(this.toggle.checked)
+    var elemento3 = document.getElementById("classTelefone");
+    var elemento = document.getElementById("classRA");
+    var elemento2 = document.getElementById("labelnao");
+    var elemento4 = document.getElementById("labelsim");
+    if(this.toggle.checked === true){
+      elemento3.hidden=true;
+      elemento.hidden=false;
+      elemento4.style.color = "green" ;
+      elemento2.style.color = "White" ;
+    } else if(this.toggle.checked === false){
+      elemento3.hidden=false;
+      elemento.hidden=true;
+      elemento4.style.color = "white" ;
+      elemento2.style.color = "green" ;
+    }
+    
+  }
+
+
 }
